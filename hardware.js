cac0231b541b4ede572c00a7ba8f0e3883903e09
Nicole Tin <ntin.sp@mysummitps.org>
2016-10-03 18:56:09 +0000

    update

--- hardware.js
+++ hardware.js
@@ -1,35 +1,37 @@
+/*global $*/
+
 $("#moboButton").click(function() {
     $('html, body').animate({
         scrollTop: $("#mother").offset().top
-    }, 2000);
+    }, 2700);
 });
 
 $("#cpuButton").click(function() {
     $('html, body').animate({
         scrollTop: $("#cpu").offset().top
-    }, 2000);
+    }, 2700);
 });
 
 $("#ramButton").click(function() {
     $('html, body').animate({
         scrollTop: $("#ram").offset().top
-    }, 2000);
+    }, 2700);
 });
 
 $("#hdButton").click(function() {
     $('html, body').animate({
         scrollTop: $("#hd").offset().top
-    }, 2000);
+    }, 2700);
 });
 
 $("#busButton").click(function() {
     $('html, body').animate({
         scrollTop: $("#bus").offset().top
-    }, 2000);
+    }, 2700);
 });
 
 $("#keyButton").click(function() {
     $('html, body').animate({
         scrollTop: $("#key").offset().top
-    }, 2000);
+    }, 2700);
 });
