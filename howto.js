8a761ffea62f14a1fc1099481df982622ffd1871
Nicole Tin <ntin.sp@mysummitps.org>
2016-10-07 08:48:12 +0000

    first commit

---
+++ howto.js
@@ -0,0 +1,46 @@
+/*global $*/
+
+$("#layButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#lay").offset().top
+    }, 2700);
+});
+
+$("#mainButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#main").offset().top
+    }, 2700);
+});
+
+$("#subButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#sub").offset().top
+    }, 2700);
+});
+
+$("#headButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#head").offset().top
+    }, 2700);
+});
+
+$("#textButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#text").offset().top
+    }, 2700);
+});
+
+$("#aesButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#aes").offset().top
+    }, 2700);
+});
+// $("#").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#").offset().top
+//     }, 2700);
+    
+// $("#").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#").offset().top
+//     }, 2700);
