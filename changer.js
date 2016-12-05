a60a70aaa7a050b72c710dbf00ea9aa67607f180
Nicole Tin <ntin.sp@mysummitps.org>
2016-12-05 19:22:41 +0000

    created color changing checkpoint

---
+++ changer.js
@@ -0,0 +1,9 @@
+// creating variables and saving DOM elements to each
+var colorinput = document.getElementById("colorinput");
+var colorbutton = document.getElementById("colorbutton");
+
+// EventLister for button.
+// Sets the body's background coloe to the user provided value
+colorbutton.addEventListener("click", function(){
+    document.body.style.backgroundColor = colorinput.value;
+})
